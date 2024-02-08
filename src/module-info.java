a80c01391183e5ec36dc243408fcbf1dc6cdcc89
riyas <riyas@10.12.37.51>
2024-02-08 10:32:52 +0530

    Bubble Sort

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,22 @@
-/**
- * 
- */
-/**
- * 
- */
-module Sorting {
+package Day1;
+
+import java.util.scanner;
+
+public class Reverse {
+	
+	public static void main (string) [] args) {
+		scanner scanner = new scanner(system.in);
+		system.out.print("Enter a number: ");
+		int number = scanner.nextInt();
+		
+		int reversedNumber = 0, remainder;
+		
+		while (number != 0) {
+			remainder = number % 10;
+			reversedNumber = reversedNumber * 10 + remainder;
+			number /= 10;
+		}
+		
+		system.out.printIn("Reversed number: " + reversedNumber);
+	}
 }
